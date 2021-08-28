ae6d4b206d27f4e9f253702866d81a0bfc6f3c89
Michael Rosenberg <michael@mrosenberg.pub>
2021-08-28 16:19:28 -0400

    Wrote root agg functionality

--- prototype/run_tests.sh
+++ prototype/run_tests.sh
@@ -7,17 +7,17 @@ set -eu
 USER_STATE="client/user-state.txt"
 USER_SERVERKEYS="client/server-keys.txt"
 AGG_STATE="aggregator/agg-state.txt"
+AGG_ROOTSTATE="aggregator/agg-root-state.txt"
 AGG_SERVERKEYS="aggregator/server-keys.txt"
 SERVER_STATE="server/server-state.txt"
-SERVER_USERINPUTS="server/user-inputs.txt"
 SERVER_SHARES="server/shares.txt"
 
 CMD_PREFIX="cargo run -- "
 
 NUM_SERVERS=1
-NUM_USERS=4
+NUM_USERS=1
 NUM_AGGREGATORS=1
-NUM_USERS_PER_AGGREGATOR=4
+NUM_USERS_PER_AGGREGATOR=1
 ROUND=1
 
 # We define four messages. "testing", "\0\0\0\0\0\0\0hello", and "\0\0\0\0\0\0\0\0\0\0\0\0world",
@@ -28,15 +28,43 @@ MSGS=";testing;\0\0\0\0\0\0\0hello;\0\0\0\0\0\0\0\0\0\0\0\0world;\0\0\0\0\0\0\0\
 # Magic command to trim the leading and trailing whitespace of its input
 TRIM="sed 's/\n$//' | sed '/^$/d'"
 
+# Checks that the given constants make sense
+check() {
+    if [[ $NUM_USERS -lt 1 ]]; then
+        echo "Error: NUM_USERS must be at least 1"
+        exit -1
+    fi
+
+    if [[ $NUM_AGGREGATORS -lt 1 ]]; then
+        echo "Error: NUM_AGGREGATORS must be at least 1"
+        exit -1
+    fi
+
+    if [[ $NUM_SERVERS -lt 1 ]]; then
+        echo "Error: NUM_SERVERS must be at least 1"
+        exit -1
+    fi
+
+    # If ceil(NUM_USERS / NUM_USERS_PER_AGGREGATOR) > NUM_AGGREGATORS, then this will fail
+    if [[ \
+        $(( ($NUM_USERS+$NUM_USERS_PER_AGGREGATOR-1) / $NUM_USERS_PER_AGGREGATOR)) \
+        -gt $NUM_AGGREGATORS \
+    ]]
+    then
+        echo "Error: NUM_USERS too high for the given NUM_AGGREGATORS"
+        exit -1
+    fi
+}
+
 # Removes all the intermediate files
 clean() {
     # The below pattern removes all files of the form "client/user-stateX.txt" for any X
     rm -f ${USER_STATE%.txt}*.txt || true
     rm -f ${USER_SERVERKEYS%.txt}*.txt || true
     rm -f ${AGG_STATE%.txt}*.txt || true
+    rm -f $AGG_ROOTSTATE || true
     rm -f ${AGG_SERVERKEYS%.txt}*.txt || true
     rm -f ${SERVER_STATE%.txt}*.txt || true
-    rm -f ${SERVER_USERINPUTS%.txt}*.txt || true
     rm -f $SERVER_SHARES || true
     echo "Cleaned"
 }
@@ -67,12 +95,14 @@ setup_aggregators() {
     # Accumulate the client registration data in this variable. The separator we use is ';'
     AGG_REGS=""
 
+    # Make new base-level aggregators and capture the registration data
     for i in $(seq 1 $NUM_AGGREGATORS); do
-        # Make a new client and capture the registration data
         STATE="${AGG_STATE%.txt}$i.txt"
         AGG_REG=$(
             $CMD_PREFIX new --agg-state "../$STATE" --server-keys "../$AGG_SERVERKEYS"
         )
+
+        # Append
         if [[ i -eq 1 ]]; then
             AGG_REGS="$AGG_REG"
         else
@@ -80,12 +110,19 @@ setup_aggregators() {
         fi
     done
 
+    # Make a new root aggregator and capture the registration data
+    AGG_REG=$(
+        $CMD_PREFIX new --agg-state "../$AGG_ROOTSTATE" --server-keys "../$AGG_SERVERKEYS"
+    )
+    AGG_REGS="$AGG_REGS;$AGG_REG"
+
     # Now do the registrations
     cd ../server
 
     # Read the regs into a variable
     IFS=';' read -ra AGG_REGS <<< "$AGG_REGS"
 
+    # Register all the aggregators
     for AGG_REG in "${AGG_REGS[@]}"; do
         echo $(seq 1 $NUM_SERVERS)
         for i in $(seq 1 $NUM_SERVERS); do
@@ -111,6 +148,7 @@ setup_clients() {
             $CMD_PREFIX new --user-state "../$STATE" --server-keys "../$USER_SERVERKEYS"
         )
 
+        # Append
         if [[ i -eq 1 ]]; then
             USER_REGS="$USER_REG"
         else
@@ -138,10 +176,13 @@ setup_clients() {
 start_round() {
     cd aggregator
 
+    # Start the base aggregators
     for i in $(seq 1 $NUM_AGGREGATORS); do
         STATE="${AGG_STATE%.txt}$i.txt"
         $CMD_PREFIX start-round --agg-state "../$STATE" --round $ROUND
     done
+    # Start the root aggregator
+    $CMD_PREFIX start-round --agg-state "../$AGG_ROOTSTATE" --round $ROUND
 
     cd ..
 }
@@ -173,6 +214,8 @@ encrypt_msgs() {
             | base64 \
             | $CMD_PREFIX encrypt-msg --user-state "../$STATE" --round $ROUND
         )
+
+        # Append
         if [[ i -eq 1 ]]; then
             CIPHERTEXTS="$CIPHERTEXT"
         else
@@ -185,23 +228,10 @@ encrypt_msgs() {
 
     cd ..
 
-    # If there are no aggregators, just save the ciphertexts in $SERVER_USERINPUTS
-    if [[ $NUM_AGGREGATORS -eq 0 ]]; then
-        cd server
-
-        touch "../$SERVER_USERINPUTS"
-        for CIPHERTEXT in "${CIPHERTEXTS[@]}"; do
-            echo $CIPHERTEXT >> "../$SERVER_USERINPUTS"
-        done
-
-        cd ..
-        return
-    fi
-
-    # If there are aggregators, then send user messages to them
+    # Then send user messages to the base-level aggregators
     cd aggregator
 
-    # Now send them to the aggregators, parititoning the users
+    # Now send them to the base-level aggregators, parititoning the users
     i=0
     CURRENT_AGG=1
     for CIPHERTEXT in "${CIPHERTEXTS[@]}"; do
@@ -220,62 +250,62 @@ encrypt_msgs() {
     cd ..
 }
 
-# Decrypts the aggregated messages using the server's secrets
-decrypt_msgs() {
-    # Collect the finalized aggregates. The separator we use is ';'
+# Propagates the base-level aggregates to the root aggregator
+propagate_aggregates() {
+    cd aggregator
+
+    # Collect the second-to-top aggregates. Currently we only support two layers
     AGGS=""
+    for i in $(seq 1 $NUM_AGGREGATORS); do
+        STATE="${AGG_STATE%.txt}$i.txt"
+        AGG=$($CMD_PREFIX finalize --agg-state "../$STATE")
 
-    # If there were no aggregators, then we just saved the encrypted client messages in the
-    # $SERVER_USERINPUTS file
-    if [[ $NUM_AGGREGATORS -eq 0 ]]; then
-        cd server
-
-        # Strip any trailing newlines. Then replace newlines with ;
-        AGGS=$(tr '\n' ';' < "../$SERVER_USERINPUTS" | sed 's/;$//')
-
-        cd ..
-    else
-        # If there are aggregators, then finalize them
-        cd aggregator
-
-        for i in $(seq 1 $NUM_AGGREGATORS); do
-            STATE="${AGG_STATE%.txt}$i.txt"
-
-            AGG=$($CMD_PREFIX finalize --agg-state "../$STATE")
-            if [[ i -eq 1 ]]; then
-                AGGS="$AGG"
-            else
-                AGGS="$AGGS;$AGG"
-            fi
-        done
-
-        cd ..
-    fi
+        # Append
+        if [[ i -eq 1 ]]; then
+            AGGS="$AGG"
+        else
+            AGGS="$AGGS;$AGG"
+        fi
+    done
 
     # Read the aggregates into an array
     IFS=';' read -ra AGGS <<< "$AGGS"
+
+    # Now input all the aggregates into the root aggregator
+    for AGG in "${AGGS[@]}"; do
+        echo "$AGG" | $CMD_PREFIX input --agg-state "../$AGG_ROOTSTATE"
+    done
+
+     cd ..
+}
+
+# Decrypts the aggregated messages using the server's secrets
+decrypt_msgs() {
+    # Finalize the root aggregator
+    cd aggregator
+    AGG=$($CMD_PREFIX finalize --agg-state "../$AGG_ROOTSTATE")
+    cd ..
 
     cd server
 
     # Create the unblinded shares file
     touch "../$SERVER_SHARES"
 
-    # Unblind the aggregates and put them all in the server share file
-    for AGG in "${AGGS[@]}"; do
-        for i in $(seq 1 $NUM_SERVERS); do
-            STATE="${SERVER_STATE%.txt}$i.txt"
-            echo $AGG \
-                | $CMD_PREFIX unblind-aggregate --server-state "../$STATE" \
-                >> "../$SERVER_SHARES"
-        done
+    # For each server, unblind the top-level aggregate and put them all in the server share file
+    for i in $(seq 1 $NUM_SERVERS); do
+        STATE="${SERVER_STATE%.txt}$i.txt"
+        echo $AGG \
+            | $CMD_PREFIX unblind-aggregate --server-state "../$STATE" \
+            >> "../$SERVER_SHARES"
     done
 
-    # Now combine the shares. It shouldn't matter which server does it
+    # Now have every combine the shares. The output should be the same for all of them.
     for i in $(seq 1 $NUM_SERVERS); do
         STATE="${SERVER_STATE%.txt}$i.txt"
         ROUND_OUTPUT=$(
             $CMD_PREFIX combine-shares --server-state "../$STATE" --shares "../$SERVER_SHARES"
         )
+
         echo -n "ROUND OUTPUT: "
         echo -n $ROUND_OUTPUT | base64 -d
         echo ""
@@ -285,9 +315,11 @@ decrypt_msgs() {
 }
 
 clean
+check
 setup_servers
 setup_aggregators
 setup_clients
 start_round
 encrypt_msgs
+propagate_aggregates
 decrypt_msgs
