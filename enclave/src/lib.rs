b403fa7d9c29379429d6d8d72422fbdfa164ce91
Rui <ruiwang6188@gmail.com>
2023-06-14 01:44:39 +0000

    ref x25519 and ed25519 in enclave pkg

--- enclave/src/lib.rs
+++ enclave/src/lib.rs
@@ -19,6 +19,9 @@ extern crate rand;
 extern crate rand_core;
 extern crate sha2;
 
+extern crate x25519_dalek;
+extern crate ed25519_dalek;
+
 #[macro_use]
 extern crate serde;
 extern crate serde_cbor;
