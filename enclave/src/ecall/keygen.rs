2444118e3de913a973186027e6a555ba0420a696
Rui <ruiwang6188@gmail.com>
2023-07-01 02:04:40 +0000

    add new_keypair_ext_internal to gen new sk and pk pair

--- enclave/src/ecall/keygen.rs
+++ enclave/src/ecall/keygen.rs
@@ -7,11 +7,6 @@ use sgx_types::SgxResult;
 use std::string::ToString;
 use std::vec;
 
-use ed25519_dalek::{
-    SecretKey,
-    PublicKey,
-};
-
 pub fn new_sgx_keypair_ext_internal(role: &str) -> SgxResult<(SgxPrivateKey, AttestedPublicKey)> {
     let mut rand = sgx_rand::SgxRng::new().map_err(|e| {
         error!("cant create rand {}", e);
@@ -29,4 +24,22 @@ pub fn new_sgx_keypair_ext_internal(role: &str) -> SgxResult<(SgxPrivateKey, Att
     Ok((sk, attested_key))
 }
 
-// pub fn new_keypair_ext_internal(role: &str) -> SgxResult<(SecretKey, )>
+pub fn new_keypair_ext_internal(role: &str) -> SgxResult<(NoSgxPrivateKey, AttestedPublicKeyNoSGX)> {
+    let mut rand = sgx_rand::SgxRng::new().map_err(|e| {
+        error!("can't create rand {}", e);
+        SGX_ERROR_UNEXPECTED
+    })?;
+
+    // generate a random secret key
+    let sk = rand.gen::<NoSgxPrivateKey>();
+    let attested_key = AttestedPublicKeyNoSGX {
+        pk: NoSgxProtectedKeyPub::try_from(&sk).map_err(|e| {
+            error!("can't generate NoSgxProtectedKeyPub from NoSgxPrivateKey: {}", e);
+            SGX_ERROR_UNEXPECTED
+        })?,
+        role: role.to_string(),
+        tee_linkable_attestation: vec![0], // TODO: add attestation
+    };
+
+    Ok((sk, attested_key))
+}
