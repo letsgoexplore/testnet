89a6a5d4811f30134c729bb224f467189779276f
ubuntuæ÷ <ubuntu@example.com>
2023-07-19 18:49:23 +0000

    modify generate_message.py for python2

--- generate_message.py
+++ generate_message.py
@@ -1,9 +1,32 @@
-import secrets
+# generate_message.py 
+# 【Purpose】 randomly generating text
+
+# not compilable for python 2
+# import secrets
+
+# def generate_random_hex(length):
+#     num_bytes = (length + 1) // 2
+#     random_bytes = secrets.token_bytes(num_bytes)
+#     random_hex = random_bytes.hex()[:length]
+#     return random_hex
+
+# def generate_round_multiple_message(user, length):
+#     for i in range(user):  
+#         random_message_hex = generate_random_hex(length)
+#         filename = "./client/src/message/clientmessage_" + str(i) + ".txt"
+#         with open(filename, "w") as file:
+#             file.write(random_message_hex)
+#         print("already saved in {}".format(filename))
+
+# # generate_round_multiple_message(2,10)
+
+import random
+import binascii
 
 def generate_random_hex(length):
     num_bytes = (length + 1) // 2
-    random_bytes = secrets.token_bytes(num_bytes)
-    random_hex = random_bytes.hex()[:length]
+    random_bytes = bytearray(random.getrandbits(8) for _ in range(num_bytes))
+    random_hex = binascii.hexlify(random_bytes)[:length]
     return random_hex
 
 def generate_round_multiple_message(user, length):
@@ -12,6 +35,5 @@ def generate_round_multiple_message(user, length):
         filename = "./client/src/message/clientmessage_" + str(i) + ".txt"
         with open(filename, "w") as file:
             file.write(random_message_hex)
-        print(f"already saved in {filename}")
-
-# generate_round_multiple_message(2,10)
+        print("already saved in {}".format(filename))
+generate_round_multiple_message(2,10)
