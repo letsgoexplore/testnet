2e45fcca36bad1347207dbbbf625262c335315d9
ubuntuæ÷ <ubuntu@example.com>
2023-07-25 16:48:54 +0000

    modify for distributed deployment(3), for testing

--- dc-net-control.sh
+++ dc-net-control.sh
@@ -1,9 +1,9 @@
 SERVER_IP=("3.15.148.53")
 SERVER_AWS_COMMANDS=("ubuntu@ec2-3-15-148-53.us-east-2.compute.amazonaws.com")
 SSH_PREFIX="ssh -t -i"
-KEY_ADDRESS="../../.ssh/dc-net-test.pem"
+KEY_ADDRESS="./dc-net-test.pem"
 TIME_LOG_ALL="server/time_recorder_all.txt"
-
+GIT_REPO="https://github.com/letsgoexplore/testnet"
 eval(){
     rm -f $TIME_LOG_ALL || true
     # num_users=("30" "60" "90" "120" "150" "180" "210")
@@ -25,7 +25,7 @@ eval(){
     for num_user in "${num_users[@]}"; do
         echo "$num_user" >> $TIME_LOG_ALL
         dc_net_n_slot=$num_user
-        clean_and_set_param_for_all
+        update_clean_and_set_param_for_all $num_server $dc_net_message_length $dc_net_n_slot
         ./server_ctrl.sh setup_env $dc_net_message_length $dc_net_n_slot $num_server $num_user
         mitigate_server_state
         start_leader
@@ -39,17 +39,22 @@ eval(){
     done
 }
 
-clean_and_set_param_for_all(){
+update_clean_and_set_param_for_all(){
     NUM_SERVERS=$1
-    $dc_net_message_length=$2
-    $dc_net_n_slot=$3
+    dc_net_message_length=$2
+    dc_net_n_slot=$3
     # clean local
     ./server_ctrl.sh clean
     
-    # clean server
+    # update and clean server 
     for i in $(seq 1 $NUM_SERVERS); do 
         SERVER_AWS_COMMAND=${SERVER_AWS_COMMANDS[$((i-1))]}
         $SSH_PREFIX $KEY_ADDRESS $SERVER_AWS_COMMAND '
+            cd dc-net/testnet
+            git add origin $GIT_REPO
+            git pull origin master
+            chmod +x ./server_ctrl.sh
+            cd  
             ./dc-net/testnet/server_ctrl.sh clean
             ./dc-net/testnet/server_ctrl.sh setup-param $dc_net_message_length $dc_net_n_slot
             exit
@@ -71,7 +76,9 @@ mitigate_server_state(){
 start_leader(){
     SERVER_AWS_COMMAND=${SERVER_AWS_COMMANDS[0]}
     $SSH_PREFIX $KEY_ADDRESS $SERVER_AWS_COMMAND '
-        ./dc-net/testnet/server_ctrl.sh start-leader
+        cd dc-net/testnet/
+        ./server_ctrl.sh start-leader
+        cd
         exit
     '
 }
@@ -81,7 +88,9 @@ start_follower(){
     for i in $(seq 1 $num_follower); do
         SERVER_AWS_COMMAND=${SERVER_AWS_COMMANDS[$i]}
         $SSH_PREFIX $KEY_ADDRESS $SERVER_AWS_COMMAND '
-            ./dc-net/testnet/server_ctrl.sh start-follower $((i+1))
+            cd dc-net/testnet/
+            ./server_ctrl.sh start-follower $((i+1))
+            cd
             exit
         '
     done
@@ -90,8 +99,8 @@ start_follower(){
 cal_time(){
     SERVER_AWS_COMMAND=${SERVER_AWS_COMMANDS[0]}
     $SSH_PREFIX $KEY_ADDRESS $SERVER_AWS_COMMAND '
-        cd ./dc-net/
-        python -c "from time_cal import time_cal; time_cal()"
+        cd dc-net/testnet/
+        python3 -c "from time_cal import time_cal; time_cal()"
         cd
         exit
     '
@@ -108,3 +117,7 @@ stop-all(){
         '
     done
 }
+
+if [[ $1 == "eval" ]]; then
+    eval
+fi
