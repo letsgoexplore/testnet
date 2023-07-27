deb5ef2c7bebc826c30ad94608bdb1a3a461a934
jasonzhao404 <1531186050@qq.com>
2023-07-27 04:23:49 +0000

    modify for distributed deployment(12.5), deal with followers
    update the new ip

--- server_ctrl.sh
+++ server_ctrl.sh
@@ -411,7 +411,7 @@ start_follower() {
     STATE="${SERVER_STATE%.txt}$1.txt"
     leader_ip=${SERVER_IP[0]}
     leader_addr="http://$leader_ip:$SERVER_PORT"
-    index=${{$1-1}}
+    index=$(($1-1))
     follower_ip=${SERVER_IP[$index]}
     follower_addr="0.0.0.0:$SERVER_PORT"
     RUST_LOG=debug $CMD_PREFIX start-service \
