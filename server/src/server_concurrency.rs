01812381e221af13487311c33a6783ee5df4d803
jasonzhao404 <1531186050@qq.com>
2023-07-30 04:46:35 +0000

    adding concurrency to server

--- server/src/server_concurrency.rs
+++ server/src/server_concurrency.rs
@@ -1,7 +1,6 @@
 use actix_web::dev::{Service, ServiceRequest, ServiceResponse, Transform};
-use actix_service::ServiceFactory;
+use actix_web::Error;
 use actix_http::body::MessageBody;
-
 use std::pin::Pin;
 use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll};
@@ -12,15 +11,15 @@ pub struct ConcurrencyLimiter;
 
 impl<S, B> Transform<S> for ConcurrencyLimiter
 where
-    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = actix_web::Error>,
-    B: actix_http::body::MessageBody,
+    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    B: MessageBody,
 {
     type Request = ServiceRequest;
     type Response = ServiceResponse<B>;
-    type Error = actix_web::Error;
+    type Error = Error;
     type InitError = ();
     type Transform = ConcurrencyLimiterMiddleware<S>;
-    type Future = std::pin::Pin<Box<dyn std::future::Future<Output = Result<Self::Transform, Self::InitError>>>>;
+    type Future = Pin<Box<dyn std::future::Future<Output = Result<Self::Transform, Self::InitError>>>>;
 
     fn new_transform(&self, service: S) -> Self::Future {
         // Wrap the service with our ConcurrencyLimiterMiddleware
@@ -37,23 +36,23 @@ struct ConcurrencyLimiterMiddleware<S> {
 
 impl<S, B> Service for ConcurrencyLimiterMiddleware<S>
 where
-    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = actix_web::Error>,
-    B: actix_http::body::MessageBody,
+    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    B: MessageBody,
 {
     type Request = ServiceRequest;
     type Response = ServiceResponse<B>;
-    type Error = actix_web::Error;
+    type Error = Error;
     type Future = Pin<Box<dyn std::future::Future<Output = Result<Self::Response, Self::Error>>>>;
 
-    fn poll_ready(&self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.service.lock().unwrap().poll_ready(cx)
     }
 
-    fn call(&self, req: ServiceRequest) -> Self::Future {
+    fn call(&mut self, req: ServiceRequest) -> Self::Future {
         // Ensure only one request is processed at a time by locking the mutex.
         let service = self.service.clone();
         Box::pin(async move {
-            let lock = service.lock().unwrap();
+            let mut lock = service.lock().unwrap();
             lock.call(req).await
         })
     }
