811a7ba4fa0302b750cdf8c94b779e24a78b683b
Rui <ruiwang6188@gmail.com>
2023-08-10 03:22:02 +0000

    retire SealedSharedSecretDb

--- enclave/src/unseal.rs
+++ enclave/src/unseal.rs
@@ -153,54 +153,6 @@ impl UnsealableInto<SgxPrivateKey> for SealedKemPrivKey {
     }
 }
 
-impl SealInto<SealedSharedSecretDb> for SharedSecretsDb {
-    fn seal_into(&self) -> SgxResult<SealedSharedSecretDb> {
-        let mut sealed_shared_secrets = SealedSharedSecretDb::default();
-        sealed_shared_secrets.round = self.round;
-
-        for (k, s) in self.db.iter() {
-            // authenticate public keys and rounds in "ad"
-            let mut ad = Vec::new();
-            ad.extend_from_slice(&k.gx);
-            ad.extend_from_slice(&k.gy);
-            ad.extend_from_slice(&self.round.to_ne_bytes());
-
-            sealed_shared_secrets
-                .db
-                .insert(k.to_owned(), s.seal(Some(&ad))?);
-        }
-
-        Ok(sealed_shared_secrets)
-    }
-}
-
-impl UnsealableInto<SharedSecretsDb> for SealedSharedSecretDb {
-    fn unseal_into(&self) -> sgx_types::SgxResult<SharedSecretsDb> {
-        let mut db = SharedSecretsDb::default();
-        db.round = self.round;
-        for (k, v) in self.db.iter() {
-            // expected ad = pk || round
-            let mut expected_ad = Vec::new();
-            expected_ad.extend_from_slice(&k.gx);
-            expected_ad.extend_from_slice(&k.gy);
-            expected_ad.extend_from_slice(&self.round.to_ne_bytes());
-
-            let (secret, ad) = unseal_vec_and_deser(&v)?;
-
-            // check that expected_ad == ad
-            if expected_ad != ad {
-                error!("unseal SharedSecretsDb failed. Ad not matching");
-                return Err(SGX_ERROR_INVALID_PARAMETER);
-            }
-
-            db.db.insert(k.to_owned(), secret);
-        }
-
-        Ok(db)
-    }
-}
-
-
 impl  SealInto<SealedSharedSecretsDbClient> for SharedSecretsDbClient {
     fn seal_into(&self) -> SgxResult<SealedSharedSecretsDbClient> {
         let mut sealed_shared_secrets = SealedSharedSecretsDbClient::default();
