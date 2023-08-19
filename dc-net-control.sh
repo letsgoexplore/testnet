111bc7b8679c575e40038b9de792f3907099bbc1
Jason.Z <1531186050@qq.com>
2023-08-19 12:22:13 -0400

    update ip

--- dc-net-control.sh
+++ dc-net-control.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SERVER_IP=("18.221.165.58" "35.180.35.198" "54.177.180.131" "43.206.224.32" "34.208.46.57" "99.79.63.232" "35.158.121.163" "13.40.111.247" "15.228.163.219" "54.210.239.112")
-SERVER_AWS_COMMANDS=("ec2-18-221-165-58.us-east-2.compute.amazonaws.com" "ec2-35-180-35-198.eu-west-3.compute.amazonaws.com" "ec2-54-177-180-131.us-west-1.compute.amazonaws.com" "ec2-43-206-224-32.ap-northeast-1.compute.amazonaws.com" "ec2-34-208-46-57.us-west-2.compute.amazonaws.com" "ec2-99-79-63-232.ca-central-1.compute.amazonaws.com" "ec2-35-158-121-163.eu-central-1.compute.amazonaws.com" "ec2-13-40-111-247.eu-west-2.compute.amazonaws.com" "ec2-15-228-163-219.sa-east-1.compute.amazonaws.com" "ec2-54-210-239-112.compute-1.amazonaws.com")
+SERVER_IP=("18.221.165.58" "18.220.71.193" "3.145.130.160" "3.142.244.232" "18.118.14.115" "52.15.215.11" "18.218.97.222" "18.117.130.105" "3.138.173.127" "3.135.64.245")
+SERVER_AWS_COMMANDS=("ec2-18-221-165-58.us-east-2.compute.amazonaws.com" "ec2-18-220-71-193.us-east-2.compute.amazonaws.com" "ec2-3-145-130-160.us-east-2.compute.amazonaws.com" "ec2-3-142-244-232.us-east-2.compute.amazonaws.com" "ec2-18-118-14-115.us-east-2.compute.amazonaws.com" "ec2-52-15-215-11.us-east-2.compute.amazonaws.com" "ec2-18-218-97-222.us-east-2.compute.amazonaws.com" "ec2-18-117-130-105.us-east-2.compute.amazonaws.com" "ec2-3-138-173-127.us-east-2.compute.amazonaws.com" "ec2-3-135-64-245.us-east-2.compute.amazonaws.com")
 AGG_AWS_COMMAND="ec2-52-15-94-176.us-east-2.compute.amazonaws.com"
 SSH_PREFIX="ssh -t -i"
 KEY_ADDRESS="./dc-net-test.pem"
