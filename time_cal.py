a32b9f3682232afb507aef566a02a269aaec9c7f
Jason.Z <1531186050@qq.com>
2023-08-18 22:13:08 -0400

    modifying cal_time file

--- time_cal.py
+++ time_cal.py
@@ -23,7 +23,9 @@ def time_cal():
     server_timestamps = read_timestamps_from_file("server/time_recorder.txt")
     
     group_num = len(server_timestamps) // 2
-    
+    agg_runtimes=[]
+    server_runtimes=[]
+    end2end_times=[]
     with open("server/result_time.txt", "a") as file:
         for i in range(group_num):
             ts1 = agg_timestamps[33*i]
@@ -35,7 +37,18 @@ def time_cal():
             server_runtime = ts4 - ts3
             end2end_time = ts4 - ts1
             
+            agg_runtimes.append(agg_runtime)
+            server_runtimes.append(server_runtime)
+            end2end_times.append(end2end_time)
+
             file.write("group" + str(i) + ":\n")
             file.write("agg_runtime:" + str(agg_runtime) + "ns\n")
             file.write("server_runtime:" + str(server_runtime) + "ns\n")
             file.write("end2end_time:" + str(end2end_time) + "ns\n")
+        agg_runtime_avg = sum(agg_runtimes)/ len(agg_runtimes)
+        server_runtime_avg = sum(server_runtimes) / len(server_runtimes)
+        end2end_time_avg = sum(end2end_times) / len(end2end_times)
+        file.write("average:\n")
+        file.write("agg_runtime:" + str(agg_runtime_avg) + "ns\n")
+        file.write("server_runtime:" + str(server_runtime_avg) + "ns\n")
+        file.write("end2end_time:" + str(end2end_time_avg) + "ns\n")
