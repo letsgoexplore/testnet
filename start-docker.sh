57d7da2a170fd991f5de5aed609622fd3294a2d1
jasonzhao404 <1531186050@qq.com>
2023-09-08 20:33:48 +0000

    updating start-docker, to expose the in-docker interface to the outer world

--- start-docker.sh
+++ start-docker.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-CONTAINER_NAME=dcnet-1.1.6
+CONTAINER_NAME=dcnet-5
 DOCKER_IMAGE=bl4ck5un/sgx-rust-fork:2004-1.1.6
 
 if docker container inspect $CONTAINER_NAME > /dev/null 2>&1; then
@@ -11,6 +11,8 @@ else
   docker run \
     -v $PWD:/root/sgx \
     -ti \
+    -p 18300:18300 \
+    -p 28942:28942 \
     --hostname $CONTAINER_NAME \
     --name $CONTAINER_NAME \
     -e SGX_MODE=SW \
