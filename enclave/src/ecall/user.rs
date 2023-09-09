ba936eac96c0819cda593c919de01998b5bc3186
jasonzhao404 <1531186050@qq.com>
2023-09-09 23:35:19 +0000

    retire new_user and new_user_batch

--- enclave/src/ecall/user.rs
+++ enclave/src/ecall/user.rs
@@ -15,48 +15,6 @@ use unseal::SealInto;
 
 use ed25519_dalek::PublicKey;
 
-/// Derives shared secrets with all the given KEM pubkeys, and derives a new signing pubkey.
-/// Returns sealed secrets, a sealed private key, and a registration message to send to an
-/// anytrust node
-pub fn new_user(
-    anytrust_server_pks: &Vec<ServerPubKeyPackage>,
-) -> SgxResult<(SealedSharedSecretDb, SealedSigPrivKey, UserRegistrationBlob)> {
-    // 1. validate the input
-    let mut kem_pks = vec![];
-    for k in anytrust_server_pks {
-        if !k.verify_attestation() {
-            return Err(SGX_ERROR_INVALID_PARAMETER);
-        }
-
-        kem_pks.push(k.kem);
-    }
-
-    let role = "user".to_string();
-
-    // 2. generate a SGX protected key. used for both signing and round key derivation
-    let (sk, pk) = new_sgx_keypair_ext_internal(&role)?;
-
-    // 3. derive server secrets
-    let server_secrets = SharedSecretsDb::derive_shared_secrets(&sk, &kem_pks)?;
-
-    debug!("DH secrets {:?}", server_secrets);
-
-    Ok((server_secrets.seal_into()?, sk.seal_into()?, pk))
-}
-
-
-pub fn new_user_batch(
-    (anytrust_server_pks, n_user): &(Vec<ServerPubKeyPackage>, usize),
-) -> SgxResult<Vec<(SealedSharedSecretDb, SealedSigPrivKey, UserRegistrationBlob)>> {
-    let mut users = vec![];
-    for _ in 0..*n_user {
-        let u = new_user(anytrust_server_pks)?;
-        users.push(u);
-    }
-
-    Ok(users)
-}
-
 /// Derives shared secrets with all the given KEM pubkeys, and derived a new signing pubkey.
 /// Returns sealed secrets, a sealed private key, and a registration message to send to an
 /// anytrust node
